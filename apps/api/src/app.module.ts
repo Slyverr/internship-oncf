eccf372e0c917a1a472ac6bf75b1276fadfd984f
Slyverr <90329317+Slyverr@users.noreply.github.com>
2026-07-26 19:37:20 +0100

    feat: register DrizzleModule in AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { DrizzleModule } from "./db/drizzle.module";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-	imports: [ConfigModule.forRoot()],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		DrizzleModule,
+		UsersModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
