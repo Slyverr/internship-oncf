6d9344f544da2e79af4f26f19212c9f2f2cdc418
Slyverr <90329317+Slyverr@users.noreply.github.com>
2026-08-02 12:16:52 +0100

    feat(users): protect endpoints with users:read permission

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param } from "@nestjs/common";
+import { Permissions } from "src/auth/permissions.decorator";
 import { UsersService } from "./users.service";
 import type { UserId } from "./users.types";
 
@@ -7,11 +8,13 @@ export class UsersController {
 	constructor(private usersService: UsersService) {}
 
 	@Get()
+	@Permissions("users:read")
 	findAll() {
 		return this.usersService.findAll();
 	}
 
 	@Get(":id")
+	@Permissions("users:read")
 	async findOneById(@Param("id") id: UserId) {
 		return this.usersService.findOneById(id);
 	}
