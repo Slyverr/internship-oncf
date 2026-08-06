03e81e0956601577285f1b3f6d8858dec681a5cc
Slyverr <90329317+Slyverr@users.noreply.github.com>
2026-08-06 10:54:18 +0100

    chore(health): replace hello-world with public health endpoint

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
 import { CustomersModule } from "./customers/customers.module";
 import { DrizzleModule } from "./db/drizzle.module";
@@ -20,6 +19,5 @@ import { UsersModule } from "./users/users.module";
 		CustomersModule,
 	],
 	controllers: [AppController],
-	providers: [AppService],
 })
 export class AppModule {}
