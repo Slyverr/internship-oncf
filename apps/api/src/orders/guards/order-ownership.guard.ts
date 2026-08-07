dbfbab6ebe704837ab896d09e13784d3f0683ff6
Slyverr <90329317+Slyverr@users.noreply.github.com>
2026-08-07 17:00:53 +0100

    feat(orders): add OrderOwnershipGuard and OrderIdPipe

---
+++ apps/api/src/orders/guards/order-ownership.guard.ts
@@ -0,0 +1,33 @@
+import {
+	CanActivate,
+	ExecutionContext,
+	ForbiddenException,
+	Injectable,
+} from "@nestjs/common";
+import { hasPermission } from "src/auth/auth.utils";
+import { Permission } from "src/db/reference-data";
+import { OrdersService } from "../orders.service";
+import { OrderIdPipe } from "../pipes/order-id.pipe";
+
+@Injectable()
+export class OrderOwnershipGuard implements CanActivate {
+	private readonly orderIdPipe = new OrderIdPipe();
+
+	constructor(private readonly ordersService: OrdersService) {}
+
+	async canActivate(context: ExecutionContext) {
+		const request = context.switchToHttp().getRequest();
+		const user = request.user;
+
+		if (!request.params.id) return true;
+		if (hasPermission(user, Permission.ORDERS_MANAGE_USER)) return true;
+
+		const id = this.orderIdPipe.transform(request.params.id);
+		const order = await this.ordersService.findOne(id);
+		if (order.userId !== user.id) {
+			throw new ForbiddenException("You can only access your own orders");
+		}
+
+		return true;
+	}
+}
