5dfbc913ddf07f5e596db274051154089c2cc522
Slyverr <90329317+Slyverr@users.noreply.github.com>
2026-08-14 22:24:17 +0100

    refactor(web): update react-query devtools button position to bottom-right

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -9,7 +9,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools buttonPosition="bottom-left"/>
+      <ReactQueryDevtools buttonPosition="bottom-right"/>
     </QueryClientProvider>
   );
 }
