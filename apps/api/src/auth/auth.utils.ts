4518fbead4c594a2db1d9b0cc613c30da077fbbe
Slyverr <90329317+Slyverr@users.noreply.github.com>
2026-08-18 01:35:26 +0100

    refactor(api/auth): rename hasPermission to hasAllPermissions and add hasAnyPermission

--- apps/api/src/auth/auth.utils.ts
+++ apps/api/src/auth/auth.utils.ts
@@ -1,9 +1,19 @@
 import { Permission } from "@ecommand/shared";
 import { AuthUser } from "./auth.types";
 
-export function hasPermission(
+export function hasAllPermissions(
 	user: AuthUser,
 	...permissions: Permission[]
 ): boolean {
 	return permissions.every((p) => user.permissions.includes(p));
 }
+
+export function hasAnyPermission(
+	user: AuthUser,
+	...permissions: Permission[]
+): boolean {
+	return permissions.some((p) => user.permissions.includes(p));
+}
+
+/** @deprecated Use hasAllPermissions instead */
+export const hasPermission = hasAllPermissions;
