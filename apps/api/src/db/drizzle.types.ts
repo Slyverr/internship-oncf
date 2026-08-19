d7444def4e1f70e0d76ff9c32e4ed2b46a9fae5c
Slyverr <90329317+Slyverr@users.noreply.github.com>
2026-08-20 00:08:49 +0100

    refactor(api/db): add Drizzle find query option types

--- apps/api/src/db/drizzle.types.ts
+++ apps/api/src/db/drizzle.types.ts
@@ -7,12 +7,20 @@ type QueryKey = keyof DrizzleDb["query"];
 
 type Query<T extends QueryKey> = DrizzleDb["query"][T];
 
-type FindManyArgs<T extends QueryKey> = Parameters<Query<T>["findMany"]>[0];
+type QueryArgs<T extends QueryKey> = Parameters<Query<T>["findMany"]>[0];
 
-type QueryOptions<T extends QueryKey> = NonNullable<FindManyArgs<T>>;
+type QueryOptions<T extends QueryKey> = NonNullable<QueryArgs<T>>;
 
 export type QueryColumns<T extends QueryKey> =
 	QueryOptions<T> extends { columns?: infer C } ? C : never;
 
 export type QueryRelations<T extends QueryKey> =
 	QueryOptions<T> extends { with?: infer W } ? W : never;
+
+export type FindManyQueryOptions<T extends QueryKey> = NonNullable<
+	Parameters<Query<T>["findMany"]>[0]
+>;
+
+export type FindFirstQueryOptions<T extends QueryKey> = NonNullable<
+	Parameters<Query<T>["findFirst"]>[0]
+>;
