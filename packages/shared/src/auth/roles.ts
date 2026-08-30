3d29909173a3860327b334b896007a55830f876c
Slyverr <90329317+Slyverr@users.noreply.github.com>
2026-08-30 14:22:21 +0100

    refactor(shared/auth): extract default role permissions

---
+++ packages/shared/src/auth/roles.ts
@@ -0,0 +1,53 @@
+import { Permission, Role } from "../enums";
+
+export const DEFAULT_ROLE_PERMISSIONS: Record<Role, Permission[] | "ALL"> = {
+	[Role.ADMIN]: "ALL",
+
+	[Role.CLIENT_REPRESENTATIVE]: [
+		Permission.ORDERS_CREATE,
+		Permission.ORDERS_READ,
+		Permission.ORDERS_UPDATE,
+		Permission.ORDERS_DELETE,
+		Permission.ORDERS_ACTION_SUBMIT,
+
+		Permission.CLAIMS_CREATE,
+		Permission.CLAIMS_READ,
+
+		Permission.REPORTS_READ,
+		Permission.PROFILE_UPDATE,
+
+		Permission.PROGRAMS_READ,
+	],
+
+	[Role.AGENT_COMMERCIAL]: [
+		Permission.ORDERS_CREATE,
+		Permission.ORDERS_READ,
+		Permission.ORDERS_UPDATE,
+		Permission.ORDERS_DELETE,
+		Permission.ORDERS_MANAGE_OTHER,
+		Permission.ORDERS_MANAGE_STATUS,
+		Permission.ORDERS_MANAGE_OWNERSHIP,
+		Permission.ORDERS_ACTION,
+
+		Permission.CUSTOMERS_READ,
+		Permission.CUSTOMERS_UPDATE,
+
+		Permission.CLAIMS_CREATE,
+		Permission.CLAIMS_READ,
+		Permission.CLAIMS_UPDATE,
+		Permission.CLAIMS_MANAGE_STATUS,
+		Permission.CLAIMS_ACTION,
+
+		Permission.TRACKING_READ,
+
+		Permission.REPORTS_READ,
+		Permission.PROFILE_UPDATE,
+
+		Permission.PROGRAMS_CREATE,
+		Permission.PROGRAMS_READ,
+		Permission.PROGRAMS_UPDATE,
+		Permission.PROGRAMS_MANAGE_STATUS,
+		Permission.PROGRAMS_MANAGE_OWNERSHIP,
+		Permission.PROGRAMS_ACTION,
+	],
+};
